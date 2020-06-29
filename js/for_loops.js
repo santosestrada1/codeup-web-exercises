ec98d1519c2851362e1b41b890dc515360b908cf
Santos Estrada <santos.estrada1@gmail.com>
2020-06-29 17:42:10 -0500

    Finished for_loops part of exercise

---
+++ js/for_loops.js
@@ -0,0 +1,28 @@
+// let x = 7;
+// y = 1;
+// while (y <= 10) {
+//     console.log(x + " * " + y + " = " + (x * y))
+//     y++;
+// }
+
+// var x = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+// for (var i = 0; i < 10; i++) {
+//
+//     if (x === 0) {
+//         console.log(x + " is even");
+//     } else if (x % 2 === 0) {
+//         console.log(x + " is even");
+//     } else {
+//         console.log(x + " is odd");
+//     }
+//     x = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+// }
+
+
+// for(i = 1; i < 10; i++) {
+//     console.log(String(i).repeat(i))
+// }
+
+for(var i=100; i>=5; i-=5) {
+    console.log(i);
+}
