ad48135586ef8da582f25eabf9f45349f529b695
Santos Estrada <santos.estrada1@gmail.com>
2020-06-30 14:13:17 -0500

    completed loops exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,18 +3,41 @@
 //     2. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
 //     3. Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
 
-let chosenOddNum = prompt("Enter an odd number between 1 and 50: ");
-for (var x=1; x<=50; x++) {
-    if (x % 2 === 0) {
+// let chosenOddNum = prompt("Enter an odd number between 1 and 50: ");
+// for (var x=1; x<=50; x++) {
+//     if (x % 2 === 0) {
+//         continue;
+//     }
+//       if (x == chosenOddNum) {
+//           console.log("Yikes Skipping Number:" + chosenOddNum)
+//           continue;
+//       }
+//     if ((x % 2) !== 0) {
+//         console.log(x)
+//     }
+// }
+
+// or
+
+let oddNumber;
+
+       while (true){
+oddNumber = prompt("Please enter an odd number between 1 and 50.");
+oddNumber = parseInt(oddNumber);
+if (oddNumber % 2 === 0 && oddNumber >= 1 && OddNumber < 50){
+    // we have a good number
+    break;
+}
+// console.log(oddNumber);
+}
+console.log("Number to skip is: " + oddNumber);
+       console.log("")
+for (let i=1; i<=50; i+=2) {
+    if (i === oddNumber) {
+        console.log("Yikes! Skipping Number " + oddNumber);
         continue;
     }
-      if (x == chosenOddNum) {
-          console.log("Yikes Skipping Number:" + chosenOddNum)
-          continue;
-      }
-    if ((x % 2) !== 0) {
-        console.log(x)
-    }
+    console.log("Here is an odd number: " + i);
 }
 
 
--- js/while.js
+++ js/while.js
@@ -1,8 +1,38 @@
 // Create a while loop that uses console.log() to create the output shown in cirriculum.
 
-var i = 1;
-// var x = 2
-while (i < 16) {
-    console.log((i*2)*i);
-    i++;
-}
+// let num = 2;
+// let i = 0
+// while (i < 16) {
+//     console.log(num);
+//     num*= 2;
+//     i++;
+// }
+
+// or
+
+// let num=1;
+// while (num < 65536){
+//     num+=2;
+//     console.log(num);
+// }
+
+
+
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    // sell cones
+// This expression will generate a random number between 1 and 5
+  let cones =  Math.floor(Math.random() * 5) + 1;
+  if (cones <= allCones) {
+      allCones = allCones - cones;
+      console.log(cones + " cones sold ...") // if there are enough cones
+  } else {
+    console.log("Cannot sell you " + cones + " cones I only have " + allCones + "...");
+  }
+} while (allCones > 0);
+
+    console.log("Yay! I sold them all!");
+
