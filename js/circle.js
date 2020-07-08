4f3b2aff6457db75b9a5e65b6eb5202af74fa53a
Santos Estrada <santos.estrada1@gmail.com>
2020-07-08 11:59:54 -0500

    Wednesday 08 July 2020

--- js/circle.js
+++ js/circle.js
@@ -9,16 +9,22 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return Math.PI * Math.pow(this.radius, 2); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-
+            var area;
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding) {
+                area = Math.round(this.getArea());
+            } else {
+                area = this.getArea();
+            }
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
         }
     };
 
@@ -30,7 +36,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
